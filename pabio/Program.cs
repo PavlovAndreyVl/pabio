a528fed1bfe60b5533c62c9dc33b97ca4f4f6ca0
Andrey Pavlov <avpavlov87@gmail.com>
2024-12-05 17:37:28 +0200

    Тесты

--- pabio/Program.cs
+++ pabio/Program.cs
@@ -1,7 +1,25 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddLocalization();
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[]
+     {
+        new CultureInfo("en"),
+        new CultureInfo("ua")
+    };
+    options.DefaultRequestCulture = new RequestCulture("ua");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
 
 var app = builder.Build();
 
@@ -17,6 +35,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+var localizationOptions = app.Services.GetService<IOptions<RequestLocalizationOptions>>().Value;
+app.UseRequestLocalization(localizationOptions);
 
 app.UseAuthorization();
 
